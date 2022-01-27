a5d658e3ba6d823fc56392279e8375ea07e41e1f
VicWell28 <vicwell.bj@gmail.com>
2022-01-26 18:49:46 -0600

    Metodo Acutalizar

--- main.py
+++ main.py
@@ -5,22 +5,29 @@ from Carpeta import *
 from Documento import *
 
 
+# #
+# #Comenzar a hacer el formulario
+
+# #
+
+
+
 ArchiveroUno = Archivero(1, "Universidad Tecnologica de Torreon")
-print(type(ArchiveroUno))
+# print(type(ArchiveroUno))
 
 Cajon1 = Cajon(1, "A")
 Cajon2 = Cajon(2, "B")
-print(type(Cajon1))
-print(type(Cajon2))
+# print(type(Cajon1))
+# print(type(Cajon2))
 
 # Cajon3 = Cajon(3, "C")
 
 Carpeta1 = Carpeta(1, "Carpeta Uno")
 Carpeta2 = Carpeta(2, "Carpeta Dos")
 Carpeta3 = Carpeta(3, "Carpeta Tres")
-print(type(Carpeta1))
-print(type(Carpeta2))
-print(type(Carpeta3))
+# print(type(Carpeta1))
+# print(type(Carpeta2))
+# print(type(Carpeta3))
 
 # Carpeta4 = Carpeta(4, "Carpeta Cuatro")
 # Carpeta5 = Carpeta(5, "Carpeta Cinco")
@@ -30,10 +37,10 @@ Archivo1 = Documento(1, "Documento Uno", "Texto" )
 Archivo2 = Documento(2, "Documento Uno", "Texto" )
 Archivo3 = Documento(3, "Documento Uno", "Texto" )
 Archivo4 = Documento(4, "Documento Uno", "Texto" )
-print(type(Archivo1))
-print(type(Archivo2))
-print(type(Archivo3))
-print(type(Archivo4))
+# print(type(Archivo1))
+# print(type(Archivo2))
+# print(type(Archivo3))
+# print(type(Archivo4))
 # Archivo5 = Documento(5, "Documento Uno", "Texto" )
 # Archivo6 = Documento(6, "Documento Uno", "Texto" )
 # Archivo7 = Documento(7, "Documento Uno", "Texto" )
@@ -61,4 +68,6 @@ Carpeta2.CreteAddDocumentos(Archivo2, Archivo4)
 # Carpeta5.CreteAddDocumentos(Archivo7, Archivo8, Archivo9)
 # Carpeta6.CreteAddDocumentos(Archivo10, Archivo11, Archivo12)
 
-ArchiveroUno.ReadArciveroAll()
+# ArchiveroUno.ReadArciveroAll()
+# ArchiveroUno.UpdateArchivero()
+Archivero.ReadArciveroAll()
